76a7a5e2e4701f856aa197b5e156be9e8fcd4074
moneyandjelly <rmsdldy12@gmail.com>
2024-09-27 15:50:17 +0900

    feat: [이근] 사용자가 카카오 로그인을 요청하면 카카오 서버로부터 사용자의 정보를 반환합니다. [MYFV-6]

--- src/auth/login/ui/KakaoLogin.vue
+++ src/auth/login/ui/KakaoLogin.vue
@@ -3,12 +3,37 @@
 </template>
 
 <script>
+import env from "@/env";
 export default {
+    data(){
+        return {
+            KAKAO_JAVASCRIPT_KEY: env.api.KAKAO_JAVASCRIPT_KEY,
+        }
+    },
     methods: {
         kakaoLoginClick() {
-            console.log("카카오 로그인 클릭");
+            window.Kakao.init(this.KAKAO_JAVASCRIPT_KEY);
+            window.Kakao.Auth.login({
+                scope: 'profile_nickname, account_email',
+                success: this.getKakaoAccount,
+            });
         },
-    }
+        getKakaoAccount() {
+            window.Kakao.API.request({
+                url: '/v2/user/me',
+                success: res => {
+                    const kakao_account = res.kakao_account;
+                    const nickname = kakao_account.profile.nickname;
+                    const email = kakao_account.email;
+                    console.log('nickname', nickname);
+                    console.log('email', email);
+                },
+                fail: error => {
+                    console.log(error);
+                }
+            });
+        },
+    },
 }
 </script>
 
