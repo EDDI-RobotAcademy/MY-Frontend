1626906d6dc6d15486fc31eccc3229bcbf690c29
moneyandjelly <rmsdldy12@gmail.com>
2024-09-27 14:46:08 +0900

    feat: [이근] 네이버 로그인 버튼 추가 [MYFV-4]

---
+++ src/auth/login/ui/NaverLogin.vue
@@ -0,0 +1,25 @@
+<template>
+    <button class="naver-login-btn" @click="naverLoginClick"></button>
+</template>
+
+<script>
+export default {
+    methods: {
+        naverLoginClick() {
+            console.log("네이버 로그인 클릭");
+        },
+    }
+}
+</script>
+
+<style scoped>
+.naver-login-btn {
+    margin: 10px 0;
+    background-image: url("@/assets/images/fixed/login/naverLogin.png");
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    height: 50px;
+    width: 200px;
+}
+</style>
