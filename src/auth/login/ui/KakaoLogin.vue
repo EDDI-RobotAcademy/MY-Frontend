b6662a46763b82b9ee4947a14dcaebf71af6fc53
moneyandjelly <rmsdldy12@gmail.com>
2024-09-27 14:00:34 +0900

    feat: [이근] 카카오 로그인 버튼 추가 [MYFV-4]

---
+++ src/auth/login/ui/KakaoLogin.vue
@@ -0,0 +1,25 @@
+<template>
+    <button class="kakao-login-btn" @click="kakaoLoginClick"></button>
+</template>
+
+<script>
+export default {
+    methods: {
+        kakaoLoginClick() {
+            console.log("카카오 로그인 클릭");
+        },
+    }
+}
+</script>
+
+<style scoped>
+.kakao-login-btn {
+    margin: 10px 0;
+    background-image: url("@/assets/images/fixed/login/kakaoLogin.png");
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    height: 50px;
+    width: 200px;
+}
+</style>
