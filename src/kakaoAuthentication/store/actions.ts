4f9cf62a6c28d228234a54e9990385a303c6a071
moneyandjelly <rmsdldy12@gmail.com>
2024-09-28 14:31:56 +0900

    feat: [이근] 사용자가 카카오 Access Token을 요청합니다. [MYFV-11]

--- src/kakaoAuthentication/store/actions.ts
+++ src/kakaoAuthentication/store/actions.ts
@@ -1,7 +1,11 @@
 import axiosInst from "@/utility/axiosInstance"
+import { ActionContext } from "vuex"
 
 export type KakaoAuthenticationActions = {
     requestKakaoOauthRedirectionToDjango(): Promise<void>
+    requestAccessTokenToDjangoRedirection(
+        context: ActionContext<any, any>,
+        payload: { code: string }): Promise<void>
 }
 
 const actions: KakaoAuthenticationActions = {
@@ -12,6 +16,20 @@ const actions: KakaoAuthenticationActions = {
             window.location.href = res.data.url
         })
     },
+    async requestAccessTokenToDjangoRedirection(
+        context: ActionContext<any, any>,
+        payload: { code: string }): Promise<void> {
+        try {
+            console.log('requestAccessTokenToDjangoRedirection()')
+            const { code } = payload
+            const response = await axiosInst.djangoAxiosInst.post(
+                'kakao_oauth/kakao/access-token', { code })
+            console.log('accessToken:', response.data.accessToken.access_token)
+        } catch (error) {
+            console.log('Access Token 요청 중 문제 발생:', error)
+            throw error
+        }
+    }
 };
 
 export default actions;
