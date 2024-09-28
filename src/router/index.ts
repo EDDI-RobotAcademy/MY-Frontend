e411851c42351de6a3be1878c847319b2a1f02f7
moneyandjelly <rmsdldy12@gmail.com>
2024-09-28 17:31:33 +0900

    feat: [이근] 사용자 성향조사를 위한 페이지를 추가합니다. [MYFV-8]

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import DashBoardRoutes from '@/dashboard/router/DashBoardRoutes'
 import KakaoAuthenticationRoutes from '@/kakaoAuthentication/router/KakaoAuthenticationRoutes'
 import GoogleAuthenticationRoutes from '@/googleAuthentication/router/GoogleAuthenticationRoutes'
+import SurveyRoutes from '@/survey/router/SurveyRoutes'
 
 
 const routes: Array<RouteRecordRaw> = [
   ...DashBoardRoutes,
   ...KakaoAuthenticationRoutes,
   ...GoogleAuthenticationRoutes,
+  ...SurveyRoutes,
 ]
 
 const router = createRouter({
