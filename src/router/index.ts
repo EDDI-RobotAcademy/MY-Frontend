ed541475bebf9c38d3788f1e4ba956227443570c
moneyandjelly <rmsdldy12@gmail.com>
2024-09-28 16:40:59 +0900

    feat: [이근] 사용자가 Google Authentication Code를 받습니다. [MYFV-13]

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import DashBoardRoutes from '@/dashboard/router/DashBoardRoutes'
 import KakaoAuthenticationRoutes from '@/kakaoAuthentication/router/KakaoAuthenticationRoutes'
 import GoogleAuthenticationRoutes from '@/googleAuthentication/router/GoogleAuthenticationRoutes'
 
 
 const routes: Array<RouteRecordRaw> = [
+  ...DashBoardRoutes,
   ...KakaoAuthenticationRoutes,
   ...GoogleAuthenticationRoutes,
 ]
