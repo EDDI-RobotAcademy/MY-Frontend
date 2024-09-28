c37bb0bb5c0f34d38fa6faa3bb83aa29a086290f
moneyandjelly <rmsdldy12@gmail.com>
2024-09-28 18:13:38 +0900

    feat: [이근] 사용자가 구글 로그인 시 구글 정보를 요청합니다. [MYFV-15]

--- src/googleAuthentication/store/actions.ts
+++ src/googleAuthentication/store/actions.ts
@@ -1,9 +1,11 @@
 import axiosInst from "@/utility/axiosInstance"
 import { ActionContext } from "vuex"
+import { AxiosResponse } from "axios"
 
 export type googleAuthenticationActions = {
     requestGoogleOauthRedirectionToDjango(): Promise<void>
     requestAccessTokenToDjangoRedirection(context: ActionContext<any, any>, payload: { code: string }): Promise<void>
+    requestUserInfoToDjango(context: ActionContext<any, any>): Promise<any>
 }
 const actions: googleAuthenticationActions = {
     async requestGoogleOauthRedirectionToDjango(): Promise<void> {
@@ -26,5 +28,19 @@ const actions: googleAuthenticationActions = {
             throw error
         }
     },
+    async requestUserInfoToDjango(
+        context: ActionContext<any, any>): Promise<any> {
+        try {
+            const accessToken = localStorage.getItem("accessToken")
+            const userInfoResponse: AxiosResponse<any> = 
+                await axiosInst.djangoAxiosInst.post('/google_oauth/google/user-info', { access_token: accessToken })
+            const userInfo = userInfoResponse.data.user_info
+            
+            return userInfo
+        } catch (error) {
+            alert('사용자 정보 가져오기 실패!')
+            throw error
+        }
+    }
 }
 export default actions;
