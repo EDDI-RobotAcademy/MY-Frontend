37ff1ca7bc59944999cbac2d1525389a63e5e94b
moneyandjelly <rmsdldy12@gmail.com>
2024-10-01 15:51:25 +0900

    feat: [이근] 사용자가 카카오 로그인 시 신규 유저면 회원가입을 진행합니다. [MYFV-31]

--- src/kakaoAuthentication/redirection/KakaoRedirection.vue
+++ src/kakaoAuthentication/redirection/KakaoRedirection.vue
@@ -11,7 +11,7 @@ export default {
     methods: {
         ...mapActions(kakaoAuthenticationModule,
             ['requestAccessTokenToDjangoRedirection', 'requestUserInfoToDjango']),
-        ...mapActions(accountModule, ['requestEmailDuplicationCheckToDjango']),
+        ...mapActions(accountModule, ['requestEmailDuplicationCheckToDjango', 'requestCreateNewAccountToDjango']),
 
         async setRedirectData() {
             const code = this.$route.query.code
@@ -28,8 +28,21 @@ export default {
             if(userInfo.kakao_account.email) {
                 const response = await this.requestEmailDuplicationCheckToDjango(userInfo.kakao_account.email)
                 console.log("기존 유저 확인", response)
+                if(!response){
+                    this.registerNewAccount(userInfo.kakao_account.email, userInfo.properties.nickname)
+                }
             }
         },
+        async registerNewAccount(email, nickname) {
+            const accountInfo = {
+                loginType: 'KAKAO',
+                email: email,
+                nickname: nickname,
+            }
+            await this.requestCreateNewAccountToDjango(accountInfo)
+            console.log('전송한 데이터:', accountInfo)
+            console.log('register submitForm email:', email)
+        }
     },
     async created() {
         await this.setRedirectData()
