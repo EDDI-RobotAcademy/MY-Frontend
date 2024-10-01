ff831c32fbcd1664cd60b2f7142baa93886faeb7
moneyandjelly <rmsdldy12@gmail.com>
2024-10-01 15:48:22 +0900

    feat: [이근] 사용자가 카카오 로그인 시 신규 유저인지 확인할 수 있습니다. [MYFV-17]

--- src/kakaoAuthentication/redirection/KakaoRedirection.vue
+++ src/kakaoAuthentication/redirection/KakaoRedirection.vue
@@ -5,17 +5,31 @@
 import { mapActions } from 'vuex'
 
 const kakaoAuthenticationModule = 'kakaoAuthenticationModule'
+const accountModule = 'accountModule'
+
 export default {
     methods: {
         ...mapActions(kakaoAuthenticationModule,
             ['requestAccessTokenToDjangoRedirection', 'requestUserInfoToDjango']),
+        ...mapActions(accountModule, ['requestEmailDuplicationCheckToDjango']),
+
         async setRedirectData() {
             const code = this.$route.query.code
             console.log('code:', code)
 
-            await this.requestAccessTokenToDjangoRedirection({ code })
-            await this.requestUserInfoToDjango()
-        }
+            const response = await this.requestAccessTokenToDjangoRedirection({ code })
+            console.log("액세스 토큰", response)
+            if(response){
+                this.checkUserExists()
+            }
+        },
+        async checkUserExists() {
+            const userInfo = await this.requestUserInfoToDjango()
+            if(userInfo.kakao_account.email) {
+                const response = await this.requestEmailDuplicationCheckToDjango(userInfo.kakao_account.email)
+                console.log("기존 유저 확인", response)
+            }
+        },
     },
     async created() {
         await this.setRedirectData()
