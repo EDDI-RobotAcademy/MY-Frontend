6c2ed56a16a069af0224065c4e815840b91f7698
HoJ00n2 <81820604+HoJ00n2@users.noreply.github.com>
2024-10-02 17:21:12 +0900

    feat: [이호준] 챗봇이 사용자 성향조사에 대한 분석결과를 보여줍니다. [MYFV-47]

---
+++ src/chatbot/store/actions.ts
@@ -0,0 +1,59 @@
+import { ActionContext } from "vuex";
+import { AxiosResponse } from "axios";
+import axiosInst from "@/utility/axiosInstance";
+import { UserInputState } from "./states";
+
+export type UserInputActions = {
+    sendSurveyToFastAPI(
+        context: ActionContext<UserInputState, any>,
+        payload: {
+            age_group: string,
+            gender: string,
+            mbti: string,
+            topic: string,
+            platform: string,
+            target_audience: string,
+            content_style: string,
+            post_frequency: string
+        }): Promise<any>
+}
+
+const actions: UserInputActions = {
+    async sendSurveyToFastAPI(
+        context: ActionContext<UserInputState, any>,
+        payload: {
+            age_group: string,
+            gender: string,
+            mbti: string,
+            topic: string,
+            platform: string,
+            target_audience: string,
+            content_style: string,
+            post_frequency: string
+        }): Promise<any> {
+        try {
+            console.log('sendSurveyToFastAPI()');
+            
+            // Payload 구조에 맞게 데이터를 준비
+            const response = await axiosInst.fastapiAxiosInst.post(
+                '/growth-strategy', {
+                    age_group: payload.age_group,
+                    gender: payload.gender,
+                    mbti: payload.mbti,
+                    topic: payload.topic,
+                    platform: payload.platform,
+                    target_audience: payload.target_audience,
+                    content_style: payload.content_style,
+                    post_frequency: payload.post_frequency
+                }
+            );
+            
+            return response.data;
+        } catch (error) {
+            console.log('sendSurveyToFastAPI() 중 문제 발생:', error);
+            throw error;
+        }
+    },
+}
+
+export default actions;
