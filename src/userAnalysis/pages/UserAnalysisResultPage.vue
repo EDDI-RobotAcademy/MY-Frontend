02322a07b282824be81b0ad32e5ae6ffc282e153
Park Yoonseo <patpark97@gmail.com>
2024-10-08 09:49:22 +0900

    feat: [박윤서] 사용자 성향 분석 결과를 웹페이지 형식으로 출력합니다. [MYFV-59]

---
+++ src/userAnalysis/pages/UserAnalysisResultPage.vue
@@ -0,0 +1,25 @@
+<template>
+    <SummaryContent>
+    </SummaryContent>
+    <MBTIContent>
+    </MBTIContent>
+  </template>
+  
+  <script>
+  import SummaryContent from '../ui/SummaryContent.vue'
+  import MBTIContent from '../ui/MBTIContent.vue'
+  export default {
+    components: {
+        SummaryContent,
+        MBTIContent
+    },
+  }
+  </script>
+  
+  <style>
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
+  
+  * {
+    font-family: 'Noto Sans KR', sans-serif;
+  }
+  </style>
