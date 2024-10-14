88c15c3f6e8aba95a14a34f96e1a8e7738aac8ab
JUNGUIHEON <jungui4366@gmail.com>
2024-10-14 17:13:51 +0900

    feat:[정의헌] 설문 응답을 시각화 하는 SurveyVisualizationPage 생성 [MYFV-78]

--- src/survey/router/SurveyRoutes.ts
+++ src/survey/router/SurveyRoutes.ts
@@ -1,6 +1,7 @@
 import SurveyPage from "@/survey/pages/SurveyPage.vue"
 import SurveyRegisterPage from "@/survey/pages/register/SurveyRegisterPage.vue"
 import SurveyDashboardPage from "@/survey/pages/dashboard/SurveyDashboardPage.vue"
+import SurveyVisualizationPage from "../pages/dashboard/SurveyVisualizationPage.vue"
 
 const SurveyRoutes = [
     {
@@ -18,6 +19,11 @@ const SurveyRoutes = [
         path: '/survey/dashboard',
         name: 'SurveyDashboardPage',
         component: SurveyDashboardPage
+    },
+    {
+        path: '/survey/visualization',
+        name: 'SurveyVisualizationPage',
+        component: SurveyVisualizationPage
     }
 ]
 
