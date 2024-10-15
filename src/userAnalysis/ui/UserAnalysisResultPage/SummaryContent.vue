8e4366b8e0ad4487f48d254eb7d063afdacf76d6
moneyandjelly <rmsdldy12@gmail.com>
2024-10-15 11:01:13 +0900

    style: [이근] 사용자 성향 분석 결과 요소 중 입력 요약 파트 UI 디자인 수정 [MYFV-62]

--- src/userAnalysis/ui/UserAnalysisResultPage/SummaryContent.vue
+++ src/userAnalysis/ui/UserAnalysisResultPage/SummaryContent.vue
@@ -1,24 +1,18 @@
 <template>
-  <div class="summary-content">
+  <div class="summary-content" ref="container">
     <div class="content-wrapper">
-      <div class="profile-info">
-        <img src="https://github.com/identicons/jasonlong.png" alt="Profile" class="profile-image" />
-        <div class="info-container">
-          <div class="name-and-button">
-            <h1 class="profile-name">사용자</h1>
-            <button :class="['subscribe-button', { 'subscribed': isSubscribed }]" @click="handleSubscribe">
-              {{ isSubscribed ? '구독 중' : '구독' }}
-            </button>
-          </div>
-          <p class="profile-stats">rmsdldy12@gmail.com · 구독자 123 · 동영상 123</p>
-          <p class="channel-info">{{ summary }}</p>
-        </div>
-      </div>
+      <h1 class="summary" ref="summary">
+        <span v-for="(sentence, index) in formattedSummary" :key="index" class="sentence">
+          {{ sentence }}
+        </span>
+      </h1>
     </div>
   </div>
 </template>
 
 <script>
+import anime from 'animejs/lib/anime.es.js';
+
 export default {
   name: 'SummaryContent',
   props: {
@@ -30,14 +24,47 @@ export default {
   data() {
     return {
       observer: null,
-      isSubscribed: false,
       summary: this.data,
     }
   },
-  methods: {
-    handleSubscribe() {
-      this.isSubscribed = !this.isSubscribed;
+  computed: {
+    formattedSummary() {
+      return this.summary.split('.').filter(sentence => sentence.trim() !== '').map(sentence => sentence.trim() + '.');
     }
+  },
+  mounted() {
+    console.log("SummaryContent 출력", this.data);
+    this.setupIntersectionObserver();
+  },
+  beforeUnmount() {
+    if (this.observer) {
+      this.observer.disconnect();
+    }
+  },
+  methods: {
+    setupIntersectionObserver() {
+      this.observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            this.animateContent();
+            this.observer.disconnect();
+          }
+        });
+      }, { threshold: 0.1 });
+      this.observer.observe(this.$refs.container);
+    },
+    animateContent() {
+      anime.timeline({
+        easing: 'easeOutExpo',
+      })
+        .add({
+          targets: this.$refs.summary.querySelectorAll('.sentence'),
+          opacity: [0, 1],
+          translateY: ['-20px', '0px'],
+          duration: 1000,
+          delay: anime.stagger(350)
+        });
+    },
   }
 }
 </script>
@@ -45,74 +72,39 @@ export default {
 <style scoped>
 .summary-content {
   background-color: #000000;
-  padding: 20px;
-  color: #ffffff;
-  height: 100vh;
+  min-height: 100vh;
   display: flex;
+  align-items: center;
+  padding: 80px 0;
 }
 
 .content-wrapper {
   max-width: 1200px;
   margin: 0 auto;
-  display: flex;
-  align-items: center;
-  padding: 20%;
-}
-
-.profile-info {
-  display: flex;
-  align-items: center;
+  padding: 0 20px;
   width: 100%;
 }
 
-.profile-image {
-  width: 150px;
-  height: 150px;
-  border-radius: 50%;
-  margin-right: 20px;
-}
-
-.info-container {
-  flex-grow: 1;
-}
-
-.name-and-button {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 10px;
-}
-
-.profile-name {
+.summary {
   font-size: 34px;
   font-weight: bold;
-  margin: 0;
-}
-
-.profile-stats {
-  font-size: 14px;
-  color: #aaaaaa;
-  margin-bottom: 8px;
-}
-
-.channel-info {
-  font-size: 14px;
-  margin-bottom: 4px;
-}
-
-.subscribe-button {
-  background-color: #ffffff;
-  color: #000000;
-  padding: 8px 16px;
-  border-radius: 9999px;
-  font-weight: bold;
-  border: none;
-  cursor: pointer;
-  transition: all 0.3s ease;
-}
-
-.subscribe-button.subscribed {
-  background-color: #333333;
   color: #ffffff;
+  margin-bottom: 40px;
+  line-height: 1.5;
+  text-align: center;
+}
+
+.sentence {
+  display: block;
+  margin-bottom: 10px;
+  opacity: 0;
+}
+
+@media (max-height: 800px) {
+  .summary-content {
+    height: auto;
+    min-height: 100vh;
+    padding: 80px 0;
+  }
 }
 </style>
