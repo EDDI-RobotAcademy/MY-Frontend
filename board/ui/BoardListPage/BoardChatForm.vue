b6e724def116c47a01cdcb11c429a6c670278b87
moneyandjelly <rmsdldy12@gmail.com>
2024-10-21 11:30:40 +0900

    feat: [이근] 사용자가 글 등록 페이지에서 채팅 영역을 볼 수 있습니다. [MYFV-105]

---
+++ board/ui/BoardListPage/BoardChatForm.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+ad
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
