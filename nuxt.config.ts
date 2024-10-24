1ee07b02dffdd0cbb179c898d793e679dc254854
HoJ00n2 <81820604+HoJ00n2@users.noreply.github.com>
2024-10-24 15:34:17 +0900

    fix: [이호준] Nuxt로 구현된 코드들을 동적 빌드합니다. [MYFV-128]

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,6 +30,10 @@ export default defineNuxtConfig({
   },
 
   vite: {
+    sever: {
+      ws: false,
+      hmr: false,
+    },
     ssr: {
       noExternal: ['vuetify'] // SSR에서도 Vuetify를 외부 패키지로 처리하지 않도록 설정
     }
