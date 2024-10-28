0d45aa791f496d36882840926376d2bdddc76b24
Park Yoonseo <patpark97@gmail.com>
2024-10-28 15:27:34 +0900

    feat: [박윤서] 사용자의 성장 히스토리를 확인할 수 있는 페이지를 추가합니다. [MYFV-154]

---
+++ growthHistory/pages/GrowthHistory.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="growth-history-container">
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+</script>
+
+<style scoped>
+.growth-history-container {
+    margin: 0 auto;
+    padding: 100px;
+    min-height: 100vh;
+    background-color: #f5f5f5;
+}
+</style>
