991bfe2c437cea5cd796e1a20e995089d1344953
moneyandjelly <rmsdldy12@gmail.com>
2024-10-31 14:35:43 +0900

    feat: [이근] 채팅 버튼위에 읽지 않은 메세지의 수를 표시합니다. [MYFV-113]

--- userAnalysis/pages/UserAnalysisPage.vue
+++ userAnalysis/pages/UserAnalysisPage.vue
@@ -142,11 +142,6 @@ onMounted(() => {
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
-
-* {
-  font-family: 'Noto Sans KR', sans-serif;
-}
 
 .main-container {
   position: relative;
